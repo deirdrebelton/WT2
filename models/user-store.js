c9678238ad3074cc300ddbec70ea7112e1a1a6d5
Glitch (phantom-recondite-dracorex) <none>
2022-08-15 09:56:24 +0000

    😬🖼 Checkpoint ./controllers/accounts.js:47063421/121 ./models/user-store.js:47063421/153

--- models/user-store.js
+++ models/user-store.js
@@ -22,6 +22,9 @@ const userStore = {
 
   getUserByEmail(email) {
     return this.store.findOneBy(this.collection, { email: email });
+  },
+  getUserByPassword(password) {
+    return this.store.findOneBy(this.collection, { password: password });
   }
 };
 
